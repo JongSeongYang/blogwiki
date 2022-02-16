ce2f826f93778de9adf2bd246761aec531db570c
jsyang <jsyang@hiandd.co.kr>
2022-02-16 17:46:37 +0900

    fix: UserController userService 사용하도록 수정

--- src/main/kotlin/com/hiandd/wiki/controller/UserController.kt
+++ src/main/kotlin/com/hiandd/wiki/controller/UserController.kt
@@ -3,20 +3,17 @@ package com.hiandd.wiki.controller
 import com.hiandd.wiki.domain.Users
 import com.hiandd.wiki.enum.ErrorCode
 import com.hiandd.wiki.errors.NoIdException
-import com.hiandd.wiki.repository.UserRepository
+import com.hiandd.wiki.service.UserService
 import com.hiandd.wiki.utils.Log
 import io.swagger.annotations.ApiOperation
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController (
-        private val userRepository: UserRepository
+        private val userService: UserService
         ) {
 
     companion object : Log {}
@@ -24,7 +21,7 @@ public class UserController (
     @ApiOperation(value = "user email 조회", notes = "user email 조회")
     @GetMapping(value = ["/{email}"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun getUserByEmail(@PathVariable email: String): ResponseEntity<Users> {
-        val findByEmail: Users = userRepository.findByEmail(email)
+        val findByEmail: Users = userService.findUserByEmail(email)
                 ?: throw NoIdException(ErrorCode.ID_NOT_FOUND)
         return ResponseEntity.ok(findByEmail)
     }
@@ -32,7 +29,7 @@ public class UserController (
     @ApiOperation(value = "all", notes = "all")
     @GetMapping(value = ["/all"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun all(): ResponseEntity<MutableList<Users>>? {
-        val findAll = userRepository.findAll()
+        val findAll = userService.findAllUserList()
         return ResponseEntity.ok(findAll)
     }
 }
